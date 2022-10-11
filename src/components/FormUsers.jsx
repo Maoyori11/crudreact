0d3adc500f982101a9e753adb4c9243377c51bff
Maoyori11 <maoyori11@gmail.com>
2022-10-10 22:50:48 -0500

    CRUD

---
+++ src/components/FormUsers.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import './styles/formUsers.css'
+
+const defaultValues ={
+    email:'',
+    password: '',
+    first_name: '',
+    last_name: '',
+    birthday: ''
+    }
+
+const FormUsers = ({createNewUser, updateInfo, updateUserById, setUpdateInfo, setFormIsClose}) => {
+
+    const {handleSubmit, reset, register} = useForm()
+
+    useEffect(() => {
+        if(updateInfo){
+        reset(updateInfo)
+        }
+    }, [updateInfo])
+    
+    const submit =  data => {
+
+        if(updateInfo){
+            // update
+            updateUserById(updateInfo.id, data)
+            setUpdateInfo()
+        }else{
+            //Create
+            createNewUser(data);
+        }
+        setFormIsClose(true)
+        reset(defaultValues)
+    }
+
+    const handleCloseForm =()=>{
+        setFormIsClose(true)
+    }
+
+    return (
+        <form className='form' onSubmit={handleSubmit(submit)}>
+            <i  onClick={handleCloseForm} className="form__x fa-sharp fa-solid fa-circle-xmark"></i>
+            <h2  className='form__title'>{updateInfo ? 'Edit user' : 'New user'}</h2>
+            <div className='form__div'>
+                <label className='form__label' htmlFor="email">E-Mail: </label>
+                <input  className='form__input' placeholder='Enter your email' type="email" id= 'email' {...register('email')}/>
+            </div>
+            <div   className='form__div'>
+                <label htmlFor="text">Password: </label>
+                <input className='form__input' placeholder='Enter your password' type="password" id= 'password' {...register('password')}/>
+            </div>
+            <div className='form__div'>
+                <label htmlFor="first_name">First Name: </label>
+                <input className='form__input' placeholder='Enter your name' type="text" id= 'first_name' {...register('first_name')}/>
+            </div>
+            <div className='form__div'>
+                <label htmlFor="last_name">Last Name: </label>
+                <input className='form__input' placeholder='Enter your las name' type="text" id= 'last_name' {...register('last_name')}/>
+            </div>
+            <div className='form__div'>
+                <label htmlFor="birthday">Birthday: </label>
+                <input className='form__input' type="date" id= 'birthday' {...register('birthday')}/>
+            </div>
+            <button className='form__btn'>{updateInfo ? 'Update' : 'Create'}</button>
+        </form>
+    )
+}
+
+export default FormUsers
